f662a2849059747d8a88d2f2a791668e350b4efb
Scrubzilla <scruburuzilla@gmail.com>
2017-09-23 17:04:24 +0200

    -Logo has been added. -Menu is finished for now.

--- TIC_Web/Site.Master.designer.cs
+++ TIC_Web/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace TIC_Web {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Logo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Logo;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
